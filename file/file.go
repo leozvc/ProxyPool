54fb4c72699e8d6e42a7bb52265ec913856b6323
leozvc <xxfs91@gmail.com>
2017-04-24 02:28:47 +0800

    file.go

--- file/file.go
+++ file/file.go
@@ -2,7 +2,8 @@ package file
 
 import (
 	"log"
-
+	"strings"
+	"io/ioutil"
 	"github.com/leozvc/ProxyPool/storage"
 )
 
@@ -11,6 +12,7 @@ import (
 
 // Run for request
 func Run() {
+
 	//for {
 	//	time.Sleep(time.Duration(2) * time.Second)
 	//自动刷新代理列表
@@ -21,11 +23,20 @@ func Run() {
 
 // ProxyHandler .
 func GetProxys() {
-
-	a := storage.ProxyAll()
-	for _, r := range a {
-
-		log.Println(r.Type, r.Data, r.ID)
+	filename := "./testfile.txt"
+ 
+	proxys := storage.ProxyAll()
+	var pl []string
+	for _, p := range proxys {
+            types := strings.Split(p.Type, ",")
+            log.Println(types[0], p.Data, p.ID)
+            s := strings.Join([]string{types[0], p.Data}, "")
+            pl = append(pl, s) 
+	}
+ 	var d1 = []byte(strings.Join(pl, "\r\n"));
+        err := ioutil.WriteFile(filename, d1, 0777)
+	if err != nil {
+	    log.Println("写入文件成功")    
 	}
 
 }
